8988198fee71520af2a3a882b48986567ee12f5d
khawkins98 <khawkins98@gmail.com>
2017-06-07 14:26:27 +0100

    Add conditional fields, validation

---
+++ static/app.js
@@ -0,0 +1,26 @@
+// websites/patterns/conditional-elements.html
+
+$( document ).ready(function() {
+  function conditionalElementsEBI(watchedParentClass) {
+    var watchedParentClass = watchedParentClass || '.conditional-form';
+
+    $(watchedParentClass).on('change', function(){
+      var activeSet = this;
+      $(activeSet).children().each(function(){
+        if ($(this).data('condition')) {
+          var conditionTarget = '#' + $(this).data('condition'),
+              conditionRequirement = $(this).data('condition-val') || 1;
+
+          if ($(conditionTarget).val() == conditionRequirement) {
+            $(this).removeClass('hidden');
+          } else {
+            $(this).addClass('hidden');
+          }
+        }
+      });
+    });
+  }
+
+  // bootstrap it
+  conditionalElementsEBI();
+});
