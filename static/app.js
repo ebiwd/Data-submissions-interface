272267ccd4e299a34c2a4867c20ab02244fe065e
khawkins98 <khawkins98@gmail.com>
2017-07-19 14:00:30 +0100

    Add conditional forms

--- static/app.js
+++ static/app.js
@@ -2,21 +2,29 @@
 $(document).ready(function() {
   function conditionalElementsEBI(watchedParentClass) {
     var watchedParentClass = watchedParentClass || '.conditional-form';
+    conditionalElementsEBI_check(watchedParentClass,$(watchedParentClass)); // initial check on bootstrap
 
     $(watchedParentClass).on('change', function(){
-      var activeSet = this;
-      $(activeSet).children().each(function(){
-        if ($(this).data('condition')) {
-          var conditionTarget = '#' + $(this).data('condition'),
-              conditionRequirement = $(this).data('condition-val') || 1;
+      conditionalElementsEBI_check(watchedParentClass,this);
+    });
+  }
 
-          if ($(conditionTarget).val() == conditionRequirement) {
-            $(this).removeClass('hidden');
-          } else {
-            $(this).addClass('hidden');
-          }
+  function conditionalElementsEBI_check(watchedParentClass,activeSet) {
+    $(activeSet).children().each(function(){
+      if ($(this).data('condition')) {
+        var conditionTarget = '#' + $(this).data('condition'),
+            conditionRequirement = $(this).data('condition-val') || 1;
+
+        // handle wild card matches
+        console.log($(conditionTarget).val());
+        if (conditionRequirement == '*' && $(conditionTarget).val() != null) {
+          $(this).removeClass('hidden');
+        } else if ($(conditionTarget).val() == conditionRequirement) {
+          $(this).removeClass('hidden');
+        } else {
+          $(this).addClass('hidden');
         }
-      });
+      }
     });
   }
 
